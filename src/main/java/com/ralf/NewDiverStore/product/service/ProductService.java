6176dbb25b9c90b7c839795f2042c1c987dbac57
Rafal <rafalpawlowski.poczta@gmail.com>
2024-04-30 01:07:32 +0200

    4. ProductRepository added in ProductService

--- src/main/java/com/ralf/NewDiverStore/product/service/ProductService.java
+++ src/main/java/com/ralf/NewDiverStore/product/service/ProductService.java
@@ -1,39 +1,59 @@
 package com.ralf.NewDiverStore.product.service;
 
 import com.ralf.NewDiverStore.product.domain.model.Product;
+import com.ralf.NewDiverStore.product.domain.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ProductService {
 
-    public Product createProduct(Product product) {
-        product.setId(UUID.randomUUID());
-        return product;
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
+    @Transactional
+    public Product createProduct(Product productRequest) {
+        Product product = new Product();
+        product.setName(productRequest.getName());
+        product.setPrice(productRequest.getPrice());
+
+        return productRepository.save(product);
+    }
+
+    @Transactional(readOnly = true)
     public List<Product> findAllProducts() {
-        List<Product> products = List.of(new Product("Diving mask", 200.00),
-                new Product("Wetsuit", 500.00),
-                new Product("Breathing device", 2040.00));
-
-        return products;
+        return productRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Product getSingleProduct(UUID id, UUID productId) {
-        return new Product("Swimming pants" + id, 15.09);
-
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            return product;
+        } else {
+            throw new EntityNotFoundException("Product with id: " + id + " not found");
+        }
     }
 
-    public Product updateProduct(UUID id, UUID productId, Product productRequest) {
+    @Transactional
+    public Product updateProduct(UUID producerId, UUID productId, Product productRequest) {
         Product product = new Product("Before update", 100.00);
         product.setName(productRequest.getName());
         return product;
     }
 
+    @Transactional
     public void deleteProduct(UUID id) {
     }
 }
