6906aafbea5e9bc3b825c961edba51bf6c13ee28
Rafal <rafalpawlowski.poczta@gmail.com>
2024-09-10 02:11:10 +0200

    38. Order operations in CartView;

--- src/main/java/com/ralf/NewDiverStore/cart/controller/CartViewController.java
+++ src/main/java/com/ralf/NewDiverStore/cart/controller/CartViewController.java
@@ -2,15 +2,16 @@ package com.ralf.NewDiverStore.cart.controller;
 
 import com.ralf.NewDiverStore.cart.domain.model.Cart;
 import com.ralf.NewDiverStore.cart.service.SessionCartService;
+import com.ralf.NewDiverStore.common.dto.Message;
 import com.ralf.NewDiverStore.product.domain.model.Product;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/order")
@@ -27,18 +28,29 @@ public class CartViewController {
     @GetMapping("/cart")
     public String index(Model model) {
 
-model.addAttribute("cart", sessionCartService.getCart());
-
+        model.addAttribute("cart", sessionCartService.getCart());
 
 
         return "cart/index";
     }
 
-@PostMapping("/increase")
-    public String increaseQuantity(){
-//TODO implement method
+
+
+    @PostMapping("/update/{productId}")
+    public String updateCartItemQuantity(@PathVariable("productId") UUID productId,
+                                         @RequestParam String action, @RequestParam int qty, Model model) {
+
+        if (action.equals("increase")) {
+            sessionCartService.addProductToCart(productId);
+
+        } else if (action.equals("decrease")) {
+            sessionCartService.removeProductFromCart(productId);
+        }
+
+        model.addAttribute("cart", sessionCartService.getCart());
         return "redirect:/order/cart";
-}
+    }
+
 
 }
 
--- src/main/java/com/ralf/NewDiverStore/cart/service/SessionCartService.java
+++ src/main/java/com/ralf/NewDiverStore/cart/service/SessionCartService.java
@@ -2,12 +2,15 @@ package com.ralf.NewDiverStore.cart.service;
 
 import com.ralf.NewDiverStore.cart.domain.model.Cart;
 import com.ralf.NewDiverStore.product.domain.model.Product;
+import com.ralf.NewDiverStore.product.domain.repository.ProductRepository;
 import com.ralf.NewDiverStore.product.service.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,8 +21,11 @@ public class SessionCartService {
 
     private final ProductService productService;
 
-    public SessionCartService(ProductService productService) {
+    private final ProductRepository productRepository;
+
+    public SessionCartService(ProductService productService, ProductRepository productRepository) {
         this.productService = productService;
+        this.productRepository = productRepository;
         this.cart = new Cart();
     }
 
@@ -31,8 +37,27 @@ public class SessionCartService {
         cart.addItem(product);
     }
 
+    public void addProductToCart(UUID productId){
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if(optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            addProductToCart(product);
+        } else{
+            throw new EntityNotFoundException("Entity with id " + productId + " not found");
+        }
+    }
+
     public void removeProductFromCart(Product product){
         cart.removeItem(product);
+    }
+ public void removeProductFromCart(UUID productId){
+     Optional<Product> optionalProduct = productRepository.findById(productId);
+     if(optionalProduct.isPresent()){
+         Product product = optionalProduct.get();
+         removeProductFromCart(product);
+     } else{
+         throw new EntityNotFoundException("Entity with id " + productId + " not found");
+     }
     }
 
     public int counter(){
